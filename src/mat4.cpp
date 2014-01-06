9c18cf08887a4d71ff1b9a1c39a3726b2f6c205d
Caleb Jones <bionicle.dragon@gmail.com>
2014-01-05 20:56:02 -0500

    Added mat4 to compilation Fixed some parameter typing that prevented compilation 	operator[] is likely useless for mat4 right now

--- src/mat4.cpp
+++ src/mat4.cpp
@@ -7,11 +7,11 @@
 mat4::mat4() : row1(0), row2(0), row3(0), row4(0) { }
 mat4::mat4(vec4 a, vec4 b, vec4 c, vec4 d) :
 			row1(a), row2(b), row3(c), row4(d) { }
-mat4::mat4(float v[]) {
+/*mat4::mat4(float v[]) {
 	for (int i = 0; i < 4; ++i) {
 		(*this)[i] = vec4(v[i*4], v[i*4+1], v[i*4+2], v[i*4+3]);
 	}
-}
+}*/
 
 // Create an identity matrix
 mat4 mat4::identity() {
@@ -23,8 +23,8 @@ mat4 mat4::identity() {
 	return m;
 }
 
-// Row indexing
-vec4& mat4::operator[](const int i) {
+// Column indexing
+vec4 mat4::operator[](const int i) {
 	if (i == 0) return vec4(row1.x, row2.x, row3.x, row4.x);
 	if (i == 1) return vec4(row1.y, row2.y, row3.y, row4.y);
 	if (i == 2) return vec4(row1.z, row2.z, row3.z, row4.z);
@@ -43,7 +43,7 @@ mat4& mat4::operator+= (const mat4 &b) {
 mat4 operator+ (mat4 a, const mat4 &b) {
 	return a += b;
 }
-mat4 mat4::operator-= (const mat4 &b) {
+mat4& mat4::operator-= (const mat4 &b) {
 	row1 -= b.row1;
 	row2 -= b.row2;
 	row3 -= b.row3;
@@ -55,7 +55,7 @@ mat4 operator- (mat4 a, const mat4 &b) {
 }
 
 // Vector-matrix multiplication (and vice-versa)
-vec4 operator* (vec4 a, const mat4 &b) {
+vec4 operator* (vec4 a, mat4 &b) {
 	vec4 c = 0;
 	c += a * b.row1;
 	c += a * b.row2;
@@ -63,22 +63,22 @@ vec4 operator* (vec4 a, const mat4 &b) {
 	c += a * b.row4;
 	return c;
 }
-vec4 operator* (const mat4 &a, const vec4 &b) {
+vec4 operator* (mat4 &a, const vec4 &b) {
 	vec4 v = 0;
-	v.x = dot(a, b.row1);
-	v.y = dot(a, b.row2);
-	v.z = dot(a, b.row3);
-	v.w = dot(a, b.row4);
+	v.x = dot(b, a.row1);
+	v.y = dot(b, a.row2);
+	v.z = dot(b, a.row3);
+	v.w = dot(b, a.row4);
 	return v;
 }
 
 // The matrix product
 mat4& mat4::operator*= (const mat4 &m) {
 	vec4 col1, col2, col3, col4;
-	col1 = *this * m[0];
-	col2 = *this * m[1];
-	col3 = *this * m[2];
-	col4 = *this * m[3];
+	col1 = *this * vec4(m.row1.x, m.row2.x, m.row3.x, m.row4.x);
+	col2 = *this * vec4(m.row1.y, m.row2.y, m.row3.y, m.row4.y);
+	col3 = *this * vec4(m.row1.z, m.row2.z, m.row3.z, m.row4.z);
+	col4 = *this * vec4(m.row1.w, m.row2.w, m.row3.w, m.row4.w);
 	this->row1 = vec4(col1.x, col2.x, col3.x, col4.x);
 	this->row2 = vec4(col1.y, col2.y, col3.y, col4.y);
 	this->row3 = vec4(col1.z, col2.z, col3.z, col4.z);
@@ -86,12 +86,12 @@ mat4& mat4::operator*= (const mat4 &m) {
 	return *this;
 }
 
-mat4 operator* (mat4 a, const mat4 &b) {
+mat4 operator* (mat4 a, mat4 &b) {
 	return a *= b;
 }
 
 // Transpose
-mat4 transpose(const mat4 &m) {
+mat4 transpose(mat4 &m) {
 	return mat4(m[0], m[1], m[2], m[3]);
 }
 
--- src/mat4.h
+++ src/mat4.h
@@ -7,9 +7,9 @@ struct mat4 {
 	vec4 row1, row2, row3, row4;
 	mat4();
 	mat4(vec4, vec4, vec4, vec4);
-	mat4(float[]);
+	//mat4(float[]);
 	mat4 identity();
-	vec4& operator[](const int i);
+	vec4 operator[] (const int i);
 	mat4& operator+= (const mat4&);
 	mat4& operator-= (const mat4&);
 	mat4& operator*= (const mat4&);
@@ -20,8 +20,9 @@ struct mat4 {
 
 mat4 operator+ (mat4, const mat4&);
 mat4 operator- (mat4, const mat4&);
-vec4 operator* (vec4, const mat4&);
-vec4 operator* (const mat4&, const vec4&);
+vec4 operator* (vec4, mat4&);
+vec4 operator* (mat4&, const vec4&);
+vec4 operator* (mat4&, mat4&);
 mat4 transpose(const mat4&);
 //mat4 mat4rotation(float, float, float, float);
 mat4 mat4translation(float, float, float);
