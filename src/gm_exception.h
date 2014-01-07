7520016803a24f3b2aeeec006cf8d8a627a53ee9
Owen Hughes <Owen@Owens-MacBook-Pro.local>
2014-01-07 14:00:54 -0500

    Fixed errors in gm_exception.h.

--- src/gm_exception.h
+++ src/gm_exception.h
@@ -1,6 +1,7 @@
 #ifndef GRAPHICS_MATH_EXCEPTION_H
 #define GRAPHICS_MATH_EXCEPTION_H
+#include <exception>
 
-struct singular_matrix: std::exception { const char* what() const throw();}
+struct singular_matrix : std::exception { const char* what() const throw();};
 	
 #endif
