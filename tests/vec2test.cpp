65a9df0cf63aef27e0402855cead50701cdf957b
Caleb Jones <bionicle.dragon@gmail.com>
2014-01-07 19:41:25 -0500

    Fixed test suite to use TAP test harness         added build-aux/tap-driver.sh Implemented TAP testing for vec2 Added vim ".swp" files to the .gitignore         Removed .asdf.swp Moved the target scripts into build-aux         .gitignore'd build-aux/ 	You'll likely have to run ./bootstrap to get your build system back Improved the bootstrap script a little bit

--- tests/vec2test.cpp
+++ tests/vec2test.cpp
@@ -3,14 +3,74 @@
 #include <stdlib.h>
 
 int main() {
+	puts("1..11"); //Show the test plan
+	
+	if (vec2(0) != vec2(0)) puts("Bail out! 1 - != inequality doesn't function");
+	else puts("ok 1 - basic inequality works");
+	if (vec2(0) == vec2(1)) puts("Bail out! 2 - == equality doesn't function");
+	else puts("ok 2 - basic equality works");	
+	if (vec2(5, 2) != vec2(5, 2)) puts("not ok 3 - two component inequality is broken");
+	else puts("ok 3 - two component inequality works");
+	if (vec2(3, 7) == vec2(1, 6)) puts("not ok 4 - two component equality is broken");
+	else puts("ok 4 - two component equality works");
+	if (vec2() != vec2(0)) puts("not ok 5 - empty constructor doesn't produce the zero vector");
+	else puts("ok 5 - empty constructor produces the zero vector");
+	if (vec2(0) != vec2(0, 0)) puts("not ok 6 - setting one component doesn't set both (tested for 0)");
+	else puts("ok 6 - setting one component sets both correctly (tested for 0)");
+	if (vec2(135) != vec2(135, 135)) puts("not ok 7 - (tested for 135)");
+	else puts("ok 7 - (tested for 135)");
 	vec2 a, b, c;
+	a = vec2(12, 14);
+	if (a.x != 12 || a.y != 14) puts("not ok 8 - vec2(x, y) constructor failed");
+	else puts("ok 8 - vec2(x, y) constructor");
+	
 	a = 0; b = 1; c = 1;
-	if (a + b != c) return 1;
+	if (a + b != c) puts("not ok 9 - simple vector addition failed");
+	else puts("ok 9 - simple vector addition");
 	a = vec2(0, 1);
 	b = vec2(4, 5);
 	c = vec2(4, 6);
-	if (a + b != c) return 2;
+	if (a + b != c) puts("not ok 10 - vector addition failed");
+	else puts("ok 10 - vector addition");
 	c = vec2(-4);
-	if (a - b != c) return 3;
+	if (a - b != c) puts("not ok 11 - vector subtraction failed");
+	else puts("ok 11 - vector subtraction");
 	return 0;
 }
+
+/*
+bool operator== (const vec2&, const vec2&);
+bool operator!= (const vec2&, const vec2&);
+struct vec2 {
+	float x, y;
+// Constructors
+	vec2();
+	vec2(float);
+	vec2(float, float);
+// Conversion constructors
+	explicit vec2(const vec4&);
+	explicit vec2(const vec3&);
+// Element indexing
+	float& operator[] (const int);
+// Arithmetic operators
+	vec2& operator+= (const vec2&);
+	vec2& operator-= (const vec2&);
+	vec2& operator*= (const vec2&);
+	vec2& operator*= (const float);
+	vec2& operator/= (const vec2&);
+	vec2& operator/= (const float);
+	vec2& operator*= (const mat2&);
+	float length();
+};
+vec2 operator+ (vec2, const vec2&);
+vec2 operator- (vec2, const vec2&);
+vec2 operator- (const vec2&);
+vec2 operator* (vec2, const vec2&);
+vec2 operator* (vec2, const float);
+vec2 operator* (const float, vec2);
+vec2 operator/ (vec2, const vec2&);
+vec2 operator/ (vec2, const float);
+vec2 operator/ (const float, vec2);
+float length(const vec2&);
+float dot(const vec2&, const vec2&);
+*/
